0ba503eec433a872ef350e8c8562cd9e22294c86
SOms3Piol <165936911+SOms3Piol@users.noreply.github.com>
2024-07-30 20:37:17 +0500

    Update Blog.jsx

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -77,8 +77,10 @@ function Block({src , title }){
     return(
         <div className='flex flex-col gap-5'>
             <div className='overflow-hidden relative group'>
-                <img className='group-hover:scale-110 transition ease-in duration-300' src={src} alt="img" />
-                <div className='group-hover:absolute z-60 bg-black top-0  opacity-0  transition ease-linear duration-500 group-hover:opacity-50 h-full w-full'></div>
+                <div>
+                     <img className='group-hover:scale-110 transition ease-in duration-300' src={src} alt="img" />
+                     <div className='group-hover:absolute z-60 bg-black top-0  opacity-0  transition ease-linear duration-500 group-hover:opacity-50 h-full w-full'></div>
+                </div>
                 <span className='absolute px-2 font-medium uppercase tracking-tighter py-2 text-xs top-4 left-4 bg-black text-white'>Jewellary</span>
             </div>
             <div className='flex flex-col gap-3'>
