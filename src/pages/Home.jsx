d361a536f33b1bbcfe7b7dbd1fe147e3d00c033a
SOms3Piol <165936911+SOms3Piol@users.noreply.github.com>
2024-08-12 11:00:40 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,17 +24,7 @@ export default function Home (){
          <TwoBlocks />
          <Reviews />
          <Blogs />
-
-         <div className="bg-[#F5F5F5] h-[250px] flex justify-center items-center">
-              <div className="flex flex-col gap-3 text-center">
-                 <h1 className="text-3xl  ">Subscribe NewsLatter</h1>
-                 <p className="text-base text-slate-400 ">Sing up to our Newsletter and get the discount code.</p>
-                 <div className="gap-2 flex">
-                    <input type="text" name="" id=""  className="border-2 border-black w-[350px] bg-transparent px-1 py-3 text-slate-400 outline-none" placeholder="Email Address"/>
-                    <button className="px-7 py-2 bg-[#222222] text-white hover:bg-black transition ease-in">Subscribe</button>
-                 </div>
-              </div>
-         </div>
+         <NewsLatter /> 
       </>
    )
 }
